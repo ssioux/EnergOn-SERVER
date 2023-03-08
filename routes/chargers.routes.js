6c602de2a1d070d6ee3c1de04a3c42374bd7fc4a
David Lazaro <dlazaro7@gmail.com>
2023-03-08 11:16:38 +0100

    chargerPointsList Route uodated

--- routes/chargers.routes.js
+++ routes/chargers.routes.js
@@ -1,31 +1,26 @@
 const router = require("express").Router();
 
-const pool = require('../db/index')
-
+const pool = require("../db/index");
 
 // GET "/chargers" => List of Chargers from DB
 router.get("/", async (req, res, next) => {
-
-  const allChargers = await pool.query('SELECT * FROM "public"."charge_point" LIMIT 1000')
-  console.log(allChargers.rows)
-
-  res.send("all chargers")
-
-  // res.json(response.rows[0].now);
-
+  const allChargers = await pool.query(
+    'SELECT * FROM "public"."charge_point" LIMIT 1000'
+  );
+  console.log(allChargers.rows);
+  res.status(200).json(allChargers.rows);
 });
 
 // GET "/chargers/:chargerId/details" => Charger details by Id from DB
 router.get("/details", async (req, res, next) => {
-    // const {chargerId} = req.params
-    // res.json(`Details of this Id:${chargerId} Charger!`);
-    const chargerById = await pool.query('SELECT * FROM "public"."charge_point_reading" LIMIT 1000')
-    console.log(chargerById.rows)
-  
-    res.send("charge point details by Id")
-  
-  });
+  // const {chargerId} = req.params
+  // res.json(`Details of this Id:${chargerId} Charger!`);
+  const chargerById = await pool.query(
+    'SELECT * FROM "public"."charge_point_reading" LIMIT 1000'
+  );
+  console.log(chargerById.rows);
 
-
+  res.send("charge point details by Id");
+});
 
 module.exports = router;
